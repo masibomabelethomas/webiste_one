f0f05501c758dc469740a4eddf0ba07726e6bcb4
masibomabelethomas <62648386+masibomabelethomas@users.noreply.github.com>
2021-09-14 04:38:38 +0200

    fast commit 2021

---
+++ script.js
@@ -0,0 +1,18 @@
+/*//alert("External Javascript")
+var date, place, massage, day;
+date = 12;
+massage = 'Find me in Masibo coffee shop';
+place = ' Kanairo.';
+day = "Monday";
+alert (massage + ' on ' + date + 'th ' + ' at ' + place );
+
+//function colorChange(){
+ //   document.getElementById('paragraph').innerHTML='ready to serve you pal';/*
+
+// conditional statements.
+if (day == "Thursday"){
+    alert('Today is '+ day);
+}
+else{
+    alert('Today is Friday');
+}/*
